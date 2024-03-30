e5fd714e4766698acb3f95c52fd75dc385c94628
EmanuelTiran <eatiran39@ga=mail.com>
2024-03-30 23:39:41 +0300

    rectangle

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,14 @@ import React from 'react'
 import Badge from './components/Badge'
 import Send from './components/Send'
 import Trash from './components/Trash'
+import Rectangle from './components/Rectangle'
 export default function App() {
   return (
     <div>
       <Send/>
       <Trash/>
-      {/* <Badge>12</Badge>  */}
-      <div style={{ display: "flex", padding: "18px",width:"800px" }}>
-        <img src="./vite.svg" alt="" />
-        <div style={{ background: "green", flexBasis:'500px',flexGrow:1}}>
-          <h3>Jessice koel</h3>
-          <p>hey Emanuel...</p>
-        </div>
-        <div style={{ background: "red",flexBasis:'50px' ,display: "flex",justifyContent:'end'}}>
-          <p>11:34</p>
-          <Badge>45</Badge>
-        </div>
-      </div>
+      <Badge/>
+      <Rectangle/>
     </div>
   )
 }
---
+++ src/components/Rectangle/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import styles from './style.module.css'
+
+
+export default function Rectangle() {
+    return (
+        <div >
+            <div className={styles.sqr}></div>
+            <div className={styles.triangle}></div>
+        </div>
+    )
+}
