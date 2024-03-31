f3c928ddf4f9a90856ee94a3977df4059f4fd6c6
EmanuelTiran <eatiran39@ga=mail.com>
2024-03-31 19:05:14 +0300

    more components

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,17 @@ import Badge from './components/Badge'
 import Send from './components/Send'
 import Trash from './components/Trash'
 import Rectangle from './components/Rectangle'
+import EmailLi from './components/EmailLi'
+import InputSearch from './components/BoxSearch'
 export default function App() {
   return (
     <div>
-      <Send/>
-      <Trash/>
-      <Badge/>
-      <Rectangle/>
+      <Send />
+      <Trash />
+      <Badge />
+      <Rectangle />
+      <EmailLi />
+      <InputSearch/>
     </div>
   )
 }
---
+++ src/components/EmailLi/index.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+import styles from './style.module.css'
+import { FaStar } from "react-icons/fa";
+
+
+export default function EmailLi({ name, subject, img }) {
+    const [flag, setFlag] = useState(true);
+
+    return (
+        <div className={styles.main}>
+            <img className={styles.image} src="https://m.media-amazon.com/images/I/31p6ZFdyjvL._AC_UF350,350_QL80_.jpg" alt="" srcset="" />
+            <div>
+                <div className={styles.name}>Kamil Boerger</div>
+                <div className={styles.subject}>i'ill send the document as soon...</div>
+            </div>
+            <div className={styles.rightSide}>
+                <div className={styles.clock}>11:43</div>
+                {flag ? <FaStar className={styles.starOn} onClick={() => setFlag(!flag)} /> : <FaStar className={styles.starOff} onClick={() => setFlag(!flag)} />}
+            </div>
+        </div>
+    )
+}
