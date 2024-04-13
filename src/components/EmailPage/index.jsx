0fcf6eb891467b8a7dba96c769d07232bba51f38
Emanuel <eatiran39@gmail.com>
2024-04-13 20:44:41 +0300

    fetch

--- src/components/EmailPage/index.jsx
+++ src/components/EmailPage/index.jsx
@@ -24,25 +24,25 @@ export default function EmailPage({ color, label }) {
     const emailData = [{ name: "Emanuel", subject: "Subject", img: "./", count: 5, id: "Emanuel" },
     { name: "Shara", subject: "whatwup dude???", img: "./", count: 65, id: "Tiran" },
     ]
-    const { loading, data, error, fetchData } = useAxiosReq({ defaultVal: {}, method: 'GET', url: emailType ,  })
+    // const { loading, data, error, fetchData } = useAxiosReq({ defaultVal: {}, method: 'GET', url: emailType ,  })
 
-    // useEffect(() => {
-    //     setDataMail([])
-    //     setLoad(true)
-    //     axios.get(`http://localhost:5050/user/${emailType}`)
-    //         .then(response => {
-    //             console.log(response)
-    //             setDataMail(response.data.chats);
-    //             console.log("response.data:", response.data);
-    //         })
-    //         .catch(error => {
-    //             console.error('Error fetching data: ', error);
-    //         }).finally(() => {
-    //             setLoad(false);
-    //         });
-    // }, [emailType]);
+    useEffect(() => {
+        setDataMail([])
+        setLoad(true)
+        axios.get(`http://localhost:5050/user/${emailType}`)
+            .then(response => {
+                console.log(response)
+                setDataMail(response.data.chats);
+                console.log("response.data:", response.data);
+            })
+            .catch(error => {
+                console.error('Error fetching data: ', error);
+            }).finally(() => {
+                setLoad(false);
+            });
+    }, [emailType]);
 
-    console.log(data);
+    // console.log(data);
 
 
 
@@ -51,13 +51,13 @@ export default function EmailPage({ color, label }) {
             <div className={`${style.container} `}>
                 <h1>{emailType} </h1>
                 <Search /><div className={`${load ? style.load : ""}`}>
-                    {data.chats && data.chats.map((mail, index) => (
+                    {dataMail && dataMail.map((mail, index) => (
                         <NavLink
                             to={`${mail._id}`}
                             className={({ isActive }) =>
                                 isActive ? style.isActive : ""
                             }
-                        ><div className={`${style.link} `}>
+                        ><div className={`${style.link}`}>
                                 <EmailLi key={index} count={mail.chat.msg.length} sender={mail.chat.msg[mail.chat.msg.length - 1].from._id} subject={mail.chat.subject} date={mail.chat.msg[mail.chat.msg.length - 1].date} className={style.liMail} />
                             </div>
                         </NavLink>
--- src/components/useAxiosReq.jsx
+++ src/components/useAxiosReq.jsx
@@ -5,15 +5,16 @@ export default function useAxiosReq({ defaultVal, method, url, body }) {
    const [data, setData] = useState(defaultVal)
    const [loading, setLoading] = useState(false)
    const [error, setError] = useState('')
-   
-   
-   console.log("bla bla bla");
+
+   // console.log("data", data);
+
    const fetchData = async () => {
       setData([])
       setLoading(true)
       try {
 
          const result = await axiosReq({ method, url, body })
+         console.log("result",result);
          setData(result)
       } catch (e) {
          setError(e)
@@ -23,5 +24,5 @@ export default function useAxiosReq({ defaultVal, method, url, body }) {
    }
    useEffect(() => { fetchData() }, [url])
 
-   return { data, loading, error , fetchData}
+   return { data, loading, error, fetchData }
 }
